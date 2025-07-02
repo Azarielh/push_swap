6b6eba5a261fee44805b47d80f06310a3cd81ed6
Azarielh <33382899+Azarielh@users.noreply.github.com>
2025-07-02 06:49:17 +0000

    .

--- srcs/libft/boolean/has_no_double.c
+++ srcs/libft/boolean/has_no_double.c
@@ -17,17 +17,16 @@ int	has_no_double(char **str)
 	int	index;
 	int	value;
 	int	temp;
-	int	error;
 	int	i;
 
 	index = 1;
 	while (str[index])
 	{
 		i = 1;
-		value = ft_atol(str[index], &error);
+		value = ft_atol(str[index]);
 		while (str[index + i])
 		{
-			temp = ft_atol(str[index + i++], &error);
+			temp = ft_atol(str[index + i++]);
 			if (value == temp)
 				return (0);
 		}
